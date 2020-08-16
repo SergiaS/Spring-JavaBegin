76e60f489329a3320fcb2bc2b94b797c54e70e66
sergias <sergias@ukr.net>
2020-08-16 19:28:56 +0300

    Beans parameters in XML file

--- src/main/java/ru/javabegin/RobotSpring/impls/robot/ModelT1000.java
+++ src/main/java/ru/javabegin/RobotSpring/impls/robot/ModelT1000.java
@@ -11,6 +11,10 @@ public class ModelT1000 implements Robot {
 	private Leg leg;
 	private Head head;
 
+	private String color;
+	private int year;
+	private boolean soundEnabled;
+
 	public ModelT1000() {
 	}
 
@@ -21,11 +25,30 @@ public class ModelT1000 implements Robot {
 		this.head = head;
 	}
 
+	public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
+		super();
+		this.hand = hand;
+		this.leg = leg;
+		this.head = head;
+		this.color = color;
+		this.year = year;
+		this.soundEnabled = soundEnabled;
+	}
+
+	public ModelT1000(String color, int year, boolean soundEnabled) {
+		this.color = color;
+		this.year = year;
+		this.soundEnabled = soundEnabled;
+	}
+
 	@Override
 	public void action() {
 		hand.catchSomething();
 		head.calc();
 		leg.go();
+		System.out.println("color: " + color);
+		System.out.println("year: " + year);
+		System.out.println("can play sound: " + soundEnabled);
 	}
 
 	@Override
@@ -55,5 +78,29 @@ public class ModelT1000 implements Robot {
 
 	public void setHead(Head head) {
 		this.head = head;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isSoundEnabled() {
+		return soundEnabled;
+	}
+
+	public void setSoundEnabled(boolean soundEnabled) {
+		this.soundEnabled = soundEnabled;
 	}
 }
