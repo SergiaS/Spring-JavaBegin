3f2b583bec0a7a1e33a995af9a7185d6d3dcd8e5
sergias <sergias@ukr.net>
2020-08-17 15:59:26 +0300

    scope: singleton and prototype

--- src/main/java/ru/javabegin/RobotSpring/main/Start.java
+++ src/main/java/ru/javabegin/RobotSpring/main/Start.java
@@ -9,8 +9,16 @@ import ru.javabegin.RobotSpring.impls.robot.ModelT1000;
 public class Start {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000_6");
-		t1000.action();
+
+		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
+		System.out.println(t1000.getHead());
+
+		t1000 = (ModelT1000) context.getBean("t1000");
+		System.out.println(t1000.getHead());
+
+		t1000 = (ModelT1000) context.getBean("t1000");
+		System.out.println(t1000.getHead());
+
 		((ConfigurableApplicationContext)context).close();// закрытие контекста вручную
 	}
 }
