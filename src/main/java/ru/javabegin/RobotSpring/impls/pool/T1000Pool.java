33541735f47da2a20d9437fd92a659b8e2f55337
sergias <sergias@ukr.net>
2020-08-19 18:27:46 +0300

    SpEL

--- src/main/java/ru/javabegin/RobotSpring/impls/pool/T1000Pool.java
+++ src/main/java/ru/javabegin/RobotSpring/impls/pool/T1000Pool.java
@@ -3,27 +3,26 @@ package ru.javabegin.RobotSpring.impls.pool;
 import ru.javabegin.RobotSpring.interfaces.Robot;
 import ru.javabegin.RobotSpring.interfaces.RobotPool;
 
-import java.util.Map;
+import java.util.Collection;
 
 public class T1000Pool implements RobotPool {
 
-	private Map<String, Robot> robotCollection;
+	private Collection<Robot> robotCollection;
 
-	public T1000Pool(Map<String, Robot> robotCollection) {
+	public T1000Pool(Collection<Robot> robotCollection) {
 		super();
 		this.robotCollection = robotCollection;
 	}
 
 	@Override
-	public Map<String, Robot> getRobotCollection() {
+	public Collection<Robot> getRobotCollection() {
 		return robotCollection;
 	}
 
-	public void action() {
-		for (Map.Entry<String, Robot> entry : robotCollection.entrySet()) {
-			System.out.println(entry.getKey());
-			entry.getValue().action();
-			System.out.println();
+	public void beginShow() {
+		for (Robot robot : robotCollection) {
+			robot.action();
+			System.out.println("=============");
 		}
 	}
 }
--- src/main/java/ru/javabegin/RobotSpring/interfaces/RobotPool.java
+++ src/main/java/ru/javabegin/RobotSpring/interfaces/RobotPool.java
@@ -1,9 +1,10 @@
 package ru.javabegin.RobotSpring.interfaces;
 
+import java.util.Collection;
 import java.util.Map;
 
 public interface RobotPool {
 
-	Map<String, Robot> getRobotCollection();
+	Collection<Robot> getRobotCollection();
 
 }
