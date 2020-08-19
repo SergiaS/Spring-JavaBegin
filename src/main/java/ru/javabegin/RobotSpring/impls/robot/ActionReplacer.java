0ed51d981ed71bc7176c0cb3f43fa738f935f574
sergias <sergias@ukr.net>
2020-08-19 12:20:01 +0300

    method replacement - HW, solution

--- src/main/java/ru/javabegin/RobotSpring/impls/robot/ActionReplacer.java
+++ src/main/java/ru/javabegin/RobotSpring/impls/robot/ActionReplacer.java
@@ -1,14 +1,14 @@
-package ru.javabegin.RobotSpring.impls.conveyor;
+package ru.javabegin.RobotSpring.impls.robot;
 
 import org.springframework.beans.factory.support.MethodReplacer;
 
 import java.lang.reflect.Method;
 
-public class ActionReplacement implements MethodReplacer {
+public class ActionReplacer implements MethodReplacer {
 
 	@Override
 	public Object reimplement(Object o, Method method, Object[] objects) throws Throwable {
 		System.out.println("BOOM");
-		return o;
+		return null;
 	}
 }
